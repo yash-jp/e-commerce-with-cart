f2439a223cf8a7872cfefafc783da536d863d274
Yash Parekh <parekhyashj@gmail.com>
2020-01-28 05:00:35 -0500

    pilot commit with page-homepage component - directory,menu-item

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = ({title,image})=>{
+  console.log(image);
+  return(
+    <div style={
+      {backgroundImage:`url(${image})`}
+    } className="menu-item">
+      <div className='content'>
+        <h1 className='title'>{title}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  )
+}
+
+export default MenuItem;
