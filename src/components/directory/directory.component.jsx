025f3aafcfb3c415aea8906347d11adff8cb96b8
Yash Parekh <parekhyashj@gmail.com>
2020-01-28 19:35:36 -0500

    large two image in homepage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -30,6 +30,7 @@ class Directory extends Component{
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
+          size:'large',
           linkUrl: 'shop/womens'
         },
         {
@@ -37,6 +38,7 @@ class Directory extends Component{
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
+          size:'large',
           linkUrl: 'shop/mens'
         }
       ]
@@ -46,8 +48,8 @@ class Directory extends Component{
   render(){
     return(
       <div className="directory-menu">
-        {this.state.sections.map(({title,imageUrl,id})=>(
-          <MenuItem key={id} title={title} image={imageUrl}/>
+        {this.state.sections.map(({title,imageUrl,id,size})=>(
+          <MenuItem key={id} title={title} image={imageUrl} size={size}/>
         ))}
       </div>
     )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title,image})=>{
-  console.log(image);
+const MenuItem = ({title,image,size})=>{
+  // console.log(image);
   return(
     <div style={
       {backgroundImage:`url(${image})`}
-    } className="menu-item">
+    } className={`menu-item ${size}`}>
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
