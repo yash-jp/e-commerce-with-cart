8e2800598fa67fbd660e4fa7bffa32f70a146c69
Yash Parekh <parekhyashj@gmail.com>
2020-01-31 01:13:14 -0500

    shop page and collection-preview component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage-component';
+import ShopPage from './pages/shop/shop.component'
 
 const HatPage = ()=>{
   return(
@@ -13,6 +14,7 @@ function App() {
     <Switch>
       <Route exact={true} path='/' component={Homepage}/>
       <Route exact={true} path='/hats' component={HatPage}/>
+      <Route exact={true} path='/shop' component={ShopPage}/>
     </Switch>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import data from './shop.data';
+
+// COMPONENTS
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component{
+    constructor(props){
+      super(props);
+      
+      this.state={
+      collections:data
+    }
+  }
+
+  render(){
+    const {collections} = this.state;
+    return (
+      <div className='shop-page'>
+        {
+          collections.map(({id,...otherCollectionProps})=>(
+            <CollectionPreview key={id} {...otherCollectionProps}/>
+          ))
+        }
+        )
+      </div>
+    )
+  }
+}
+
+export default ShopPage;
