7ee5cdea1795ce9e93ea7207a630296ffce57850
Yash Parekh <parekhyashj@gmail.com>
2020-01-31 01:15:40 -0500

    4 items inside the collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,9 @@ const CollectionPreview = ({title,items})=>{
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
-        {items.map((item)=>(
+        {items
+        .filter((item,idx)=>idx<4)
+        .map((item)=>(
           <div key={item.id}>
             {item.name}
           </div>
