99445950eec45592278611c1b0a067c3d40ba2e7
Yash Parekh <parekhyashj@gmail.com>
2020-01-31 10:00:21 -0500

    inclusion of header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage-component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatPage = ()=>{
   return(
@@ -11,11 +12,14 @@ const HatPage = ()=>{
 
 function App() {
   return (
-    <Switch>
-      <Route exact={true} path='/' component={Homepage}/>
-      <Route exact={true} path='/hats' component={HatPage}/>
-      <Route exact={true} path='/shop' component={ShopPage}/>
-    </Switch>
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact={true} path='/' component={Homepage}/>
+        <Route exact={true} path='/hats' component={HatPage}/>
+        <Route exact={true} path='/shop' component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './collection-item.styles.scss';
-import CollectionPreview from '../collection-preview/collection-preview.component';
 
 const CollectionItem = ({id,name,price,imageUrl})=>(
   <div className='collection-item'>
