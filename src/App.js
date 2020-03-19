088b420ae649169c71db2e1c92673c3966c95b34
Yash Parekh <parekhyashj@gmail.com>
2020-03-19 16:28:24 -0400

    homepage with its components directory, menuitem

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import {Route,Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage-component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
-const HatPage = ()=>{
-  return(
-    <h1>Hats page!</h1>
-  )
-}
+
+
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact={true} path='/' component={Homepage}/>
-        <Route exact={true} path='/hats' component={HatPage}/>
-        <Route exact={true} path='/shop' component={ShopPage}/>
-        <Route exact={true} path='/signin' component={SignInAndSignUpPage}/>
-      </Switch>
-    </div>
+    <Homepage/>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import './menu-item.styles.scss';
+import React from "react";
+import "./menu-item.styles.scss";
 
-const MenuItem = ({title,image,size,history,linkUrl,match})=>{
-  console.log(linkUrl);
-  return(
-    <div style={
-      {backgroundImage:`url(${image})`}
-    } className={`menu-item ${size}`}
-    onClick={()=>history.push(`${match.url}${linkUrl}`)}>
-      <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
+const MenuItem = ({ title, imageUrl,size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className={`${size} menu-item`}
+  >
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-  )
-}
+  </div>
+);
 
-export default withRouter(MenuItem);
+export default MenuItem;
