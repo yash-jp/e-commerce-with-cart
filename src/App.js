e053c70e76fc2dbfb4d1c3b5e0298a2c72f64312
Yash Parekh <parekhyashj@gmail.com>
2020-03-21 18:20:18 -0400

    added Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop-component';
-import {Route} from 'react-router-dom';
+import Header from './components/header/header.component';
+import {Route,Switch} from 'react-router-dom';
 
 import './App.css';
 
@@ -10,9 +11,12 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
       <Route exact path='/' component={Homepage}/>
      
       <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './header.styles.scss';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import {ReactComponent as Logo} from '../../assets/crown.svg'
 
-const Header = ()=>{
-  return(
-    <div className='header'>
-      <Link to='/'>
-        <Logo className='logo'/>
+const Header = ()=>(
+  <div className='header'>
+    <Link to = '/' className='logo-container'>
+      <Logo className='logo'></Logo>
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
       </Link>
-      <div className='options'>
-        <Link className='option' to='/shop'>SHOP</Link>
-        <Link className='option' to='/shop'>CONTACT</Link>
-      </div>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
     </div>
-  )
-}
+  </div>
+);
 
 export default Header;
